2808ee476361444c1a58e32368a555b9d85061c6
mahoshojoHCG <yzy688@gmail.com>
2021-01-29 11:45:00 +0800

    Fix package

--- package_dpkg.sh
+++ package_dpkg.sh
@@ -7,4 +7,8 @@ dotnet publish -c release -r linux-x64 --no-self-contained
 cd ./bin/release/net5.0/linux-x64/publish
 PREFIX=../../../../../../package/dpkg ./install.sh
 cd ../../../../../..
+cd AutoReport
+dotnet publish -c release -r linux-x64 --no-self-contained
+cp ./bin/release/net5.0/linux-x64/publish/*.json ../package/dpkg/usr/share/AutoReport
+cd ..
 dpkg-deb -b package/dpkg hit-autoreport-debian-amd64.deb
--- package_tar.sh
+++ package_tar.sh
@@ -21,6 +21,9 @@ mv ./bin/release/net5.0/win-x64/publish ../package/win-x64-no-self-contained
 cd ../AutoReport
 dotnet publish -c release -r win-x64
 cp ./bin/release/net5.0/win-x64/publish/*.exe ../package/win-x64-no-self-contained
+cp ./bin/release/net5.0/win-x64/publish/*.json ../package/win-x64-no-self-contained
+dotnet publish -c release -r linux-x64 --no-self-contained
+cp ./bin/release/net5.0/linux-x64/publish/*.json ../package/linux-x64-no-self-contained
 
 cd ../package
 tar -cJvf hit-autoreport-linux-x64-self-contained.tar.xz linux-x64-self-contained
